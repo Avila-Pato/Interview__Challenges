4cf1118c35c877779c94382042fba0aeb306572c
Avila-Pato <p.avilaf1998@gmail.com>
2026-06-23 15:34:06 -0400

    datos

--- app/algoritmos/sesion-1/transformador/index.ts
+++ app/algoritmos/sesion-1/transformador/index.ts
@@ -17,3 +17,13 @@ export default function transformador(input: Input): Output[] {
   }))
   
 }
+
+transformador({
+  nombres: ["Bruno", "Andrea"],
+  edades: [20, 19],
+});
+
+// [
+//   { id: 1, nombre: "Bruno", edad: 20 },
+//   { id: 2, nombre: "Andrea", edad: 19 },
+// ]
