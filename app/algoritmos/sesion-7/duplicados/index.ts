5d6898fe8fa0ddcf428eb1390d7f87b6234cf601
Avila-Pato <p.avilaf1998@gmail.com>
2026-07-08 17:38:20 -0400

    Duplicados

--- app/algoritmos/sesion-7/duplicados/index.ts
+++ app/algoritmos/sesion-7/duplicados/index.ts
@@ -1,3 +1,15 @@
 export default function tieneDuplicados(array: string[]): boolean {
+
+    //   return new Set(array).size !== array.length;
+
     
+    const vistos = new Set<string>()
+
+    for (let i = 0; i < array.length; i++) {
+        if(vistos.has(array[i])) {
+            return true // duplciado encontrado
+        }
+        vistos.add(array[i])
+    }
+    return false
 }
