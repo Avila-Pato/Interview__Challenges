fe846eec3ed0f67ad02d0e797c61b425814302b0
Avila-Pato <p.avilaf1998@gmail.com>
2026-07-08 19:40:03 -0400

    SUMA DE RANGOS

--- app/algoritmos/sesion-7/numeros-perdidos/index.ts
+++ app/algoritmos/sesion-7/numeros-perdidos/index.ts
@@ -1,4 +1,14 @@
 export default function numerosPerdidos(numeros: number[]): number[] {
-  // TODO: implementar
-  return [];
+  const faltantes = []
+  const max = Math.max(...numeros)
+
+  for (let i = 1; i <= max; i++) {
+    if (!numeros.includes(i)) {
+      faltantes.push(i)
+    }
+  }
+  return faltantes
 }
+
+const numeros = [1, 2, 4, 6, 7, 9]
+console.log(numerosPerdidos(numeros)) // [3, 5, 8]
--- app/algoritmos/sesion-7/suma-de-rangos/index.ts
+++ app/algoritmos/sesion-7/suma-de-rangos/index.ts
@@ -1,4 +1,18 @@
 export default function sumaDeRangos(inicio: number, fin: number): number {
-  // TODO: implementar
-  return 0;
+  if(inicio > fin) return 0
+
+  let suma = 0;
+  for (let i = inicio; i <= fin; i++) {
+    suma += i;
+  }
+  return suma;
 }
+
+const resultado = sumaDeRangos(1, 5);
+console.log(`La suma de los números del 1 al 5 es: ${resultado}`); // La suma de los números del 1 al 5 es: 15
+
+const resultado2 = sumaDeRangos(5, 1);
+console.log(`La suma de los números del 5 al 1 es: ${resultado2}`); // La suma de los números del 5 al 1 es: 0
+
+const resultado3 = sumaDeRangos(3, 7);
+console.log(`La suma de los números del 3 al 7 es: ${resultado3}`); // La suma de los números del 3 al 7 es: 25
