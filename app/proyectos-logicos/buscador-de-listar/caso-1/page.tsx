27456c2e77ca614964b1e578139697971950aa01
Avila-Pato <p.avilaf1998@gmail.com>
2026-07-13 13:52:42 -0400

    CASO 1 listo

--- app/proyectos-logicos/buscador-de-listar/caso-1/page.tsx
+++ app/proyectos-logicos/buscador-de-listar/caso-1/page.tsx
@@ -1,29 +1,55 @@
-
+"use client";
 import {useEffect, useState} from "react";
 
 import api from "./api";
 import { Product } from "./type";
 
+type Sort = "asc" | "desc"; 
+
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
   const [query, setQuery] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(true);
+ 
+  // filtros sorted Products
+  const [sort, setSort] = useState<Sort>("asc");
+ 
+  const sortedProduct = products.sort((a, b) => {
+    if (sort === "asc") {
+      return a.price - b.price;
+    } else {
+      return b.price - a.price;
+    }
+  })
 
   useEffect(() => {
-    api.search(query).then(setProducts);
+    api.search(query).then(setProducts).finally(() => setLoading(false));
   }, [query]);
+
+ 
+  //Formater para mostrar el precio argentino 
+  const formater = new Intl.NumberFormat("es-AR", { style: "currency", currency: "ARS" }); 
 
   return (
     <main>
       <h1>Tienda digitaloncy</h1>
       <input name="text" placeholder="tv" type="text" onChange={(e) => setQuery(e.target.value)} />
+      <select onChange={(e) => setSort(e.target.value as Sort)}>
+        <option value="asc">Ascendente</option>
+        <option value="desc">Descendente</option>
+      </select>
       <ul>
-        {products.map((product) => (
+        { loading ? (
+          <li>Cargando...</li>
+        ): (
+        sortedProduct.map((product) => (
           <li key={product.id}>
             <h4>{product.title}</h4>
             <p>{product.description}</p>
-            <span>$ {product.price}</span>
+            <span>$ {formater.format(product.price)}</span>
           </li>
-        ))}
+        ))
+        )}
       </ul>
     </main>
   );
