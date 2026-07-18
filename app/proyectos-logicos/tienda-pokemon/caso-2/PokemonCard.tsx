c081f7aa16419561f079d234706a85d19d46221e
Avila-Pato <p.avilaf1998@gmail.com>
2026-07-18 19:25:55 -0400

    CASO 2 POR Hacer tarea 3 y extra

--- app/proyectos-logicos/tienda-pokemon/caso-2/PokemonCard.tsx
+++ app/proyectos-logicos/tienda-pokemon/caso-2/PokemonCard.tsx
@@ -2,10 +2,12 @@ import type {Pokemon} from "./types";
 
 type PokemonCardProps = {
   pokemon: Pokemon;
+  isInCart: (id: number | string) => boolean;
   onAdd: (pokemon: Pokemon) => void;
+  onRemove: (pokemon: Pokemon) => void;
 };
 
-export default function PokemonCard({pokemon, onAdd}: PokemonCardProps) {
+export default function PokemonCard({pokemon, onAdd, onRemove, isInCart}: PokemonCardProps) {
   return (
     <article key={pokemon.id}>
       <img className="nes-container" src={pokemon.image} />
@@ -13,8 +15,12 @@ export default function PokemonCard({pokemon, onAdd}: PokemonCardProps) {
         <p>{pokemon.name}</p>
         <p>{pokemon.description}</p>
       </div>
-      <button className="nes-btn" onClick={() => onAdd(pokemon)}>
-        Agregar
+     <button 
+        type="button" 
+        className={`nes-btn ${isInCart(pokemon.id) ? "is-error" : "is-primary"}`} 
+        onClick={() => isInCart(pokemon.id) ? onRemove(pokemon) : onAdd(pokemon)}
+      >
+        {isInCart(pokemon.id) ? "Quitar" : "Agregar"}
       </button>
     </article>
   );
--- app/proyectos-logicos/tienda-pokemon/caso-2/page.tsx
+++ app/proyectos-logicos/tienda-pokemon/caso-2/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import type {Pokemon} from "./types";
 
 import {useState} from "react";
@@ -8,25 +9,50 @@ import PokemonCard from "./PokemonCard";
 function App() {
   const [cart, setCart] = useState<Pokemon[]>([]);
 
+  const totalMoney = cart.reduce((total, pokemons) => total + pokemons.price, 0);
+
+
   return (
     <>
       <nav>
         <input className="nes-input" id="name_field" placeholder="Charmander" type="text" />
       </nav>
-      <section>
-        {POKEMONS.map((pokemon) => (
-          <PokemonCard
-            key={pokemon.id}
-            pokemon={pokemon}
-            onAdd={() => setCart(cart.concat(pokemon))}
-          />
-        ))}
-      </section>
-      <aside>
-        <button className="nes-btn is-primary">0 items (total $0)</button>
+      <section style={styles.section}>
+        {POKEMONS.map((pokemon) => {
+          const isInCart = (id: string | number) => cart.some((p) => p.id === id);
+
+          return (
+            <PokemonCard
+              key={pokemon.id}
+              pokemon={pokemon}
+              isInCart={isInCart}
+              onAdd={() => setCart((prev) => {
+                if (prev.some((p) => p.id === pokemon.id)) {
+                  return prev;
+                }
+                return [...prev, pokemon];
+              })}
+              onRemove={() => setCart((prev) => prev.filter((p) => p.id !== pokemon.id))}
+            />
+          );
+        })}
+          <aside>
+        <button className="nes-btn is-primary"> tienes {cart.length}</button>
+        <button className="nes-btn is-primary"> total ${totalMoney}</button>
       </aside>
+      </section>
+    
     </>
   );
 }
 
 export default App;
+
+const styles: Record<string, React.CSSProperties> = {
+  section: {
+    display: "grid",
+    gridTemplateColumns: "repeat(5, 1fr)",
+    gap: "1rem",
+    padding: "1rem",
+  },
+};
